405cd43a233f057467e0e0e8d35cf0e738449484
MrVictor42 <guin-hu@hotmail.com>
2020-04-22 17:46:56 -0300

    React dom

--- src/App.js
+++ src/App.js
@@ -1,62 +1,28 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
-import List from './List';
+import routesConfig from './routesConfig';
 
 class App extends Component {
 
-	constructor(props) {
-		super(props)
-	
-		this.state = {
-			name: 'Victor',
-			email: 'victor.mota@gmail.com'
-		}
-
-		this.changeState = this.changeState.bind(this);
-		this.resetState = this.resetState.bind(this);
-		this.changeInput = this.changeInput.bind(this);
-	}
-
-	changeState() {
-		this.setState({ name: 'Victor Mota' });
-	}
-
-	resetState() {
-		this.setState({ name: 'Victor' });
-	}
-
-	changeInput(event) {
-		let target = event.target;
-		let index = target.name;
-		this.setState({ [index]: target.value })
-	}
-
 	render() {
 		return (
-			<div className="App">
-				<div>
-					<form>
-						<label> Nome: 
-							<input 
-								type = 'text' 
-								name = 'name' 
-								value = { this.state.name } 
-								onChange = { this.changeInput }/>
-						</label>
-						<label> Email:
-							<input 
-								type = 'email' 
-								name = 'email' 
-								value = { this.state.email }
-								onChange = { this.changeInput }
-								/>
-						</label>
-					</form>
-					{ this.state.name } -> { this.state.email }
+			<div>
+				<div className="App">
+					<Link to = '/'> Home </Link>
+					<Link to = '/user'> User </Link>	
 				</div>
-				<button onClick = { this.changeState }> Mudar Estado </button>
-				<button onClick = { this.resetState }> Reset Estado </button>
-				<List/>
+			{ routesConfig.map((value, key) => {
+				return (
+					<Route 
+						key = { key } 
+						path = { value.path } 
+						component = { value.component }
+						exact = { value.exact }
+					>
+					</Route>
+				) 
+			})}
 			</div>	 
 		);
 	}
